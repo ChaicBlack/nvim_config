270f48e12d297ae04257cba675deacb294c4fce3
Bruce <18805656527@163.com>
2024-12-18 01:03:51 +0800

    updated

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'brenoprata10/nvim-highlight-colors',
+    config = function()
+      require('nvim-highlight-colors').setup {
+        render = 'virtual', -- 在旁边显示颜色块（或使用 'background' 直接填充背景）
+        enable_named_colors = true, -- 支持颜色名称（如 red, blue）
+        enable_tailwind = true, -- 支持 Tailwind CSS 颜色
+        formatting = {
+          format = require('nvim-highlight-colors').format,
+        },
+      }
+    end,
+  },
+}
